b6d2191526da1fe686f77a052d7d3ec3538fd2f8
Kharidiron <kharidiron@gmail.com>
2016-07-12 01:09:34 -0700

    Updated plugins to have more verbose documentation, and cleaner/clearer code.

--- plugins/chat_manager.py
+++ plugins/chat_manager.py
@@ -1,7 +1,18 @@
+"""
+StarryPy Chat Manager Plugin
+
+Provides core chat management features, such as mute... and that's it right now.
+
+Original authors: AMorporkian
+Updated for release: kharidiron
+"""
+
 from base_plugin import SimpleCommandPlugin
 from plugins.player_manager import Moderator
 from utilities import Command, send_message
 
+
+# Roles
 
 class Mute(Moderator):
     pass
@@ -19,9 +30,15 @@ class UnmutePlayer(Mute):
     pass
 
 
+###
+
 class ChatManager(SimpleCommandPlugin):
     name = "chat_manager"
-    depends = ['player_manager', 'command_dispatcher']
+    depends = ["player_manager", "command_dispatcher"]
+
+    def __init__(self):
+        super().__init__()
+        self.storage = None
 
     def activate(self):
         super().activate()
@@ -29,52 +46,77 @@ class ChatManager(SimpleCommandPlugin):
         if "mutes" not in self.storage:
             self.storage["mutes"] = set()
 
-    def on_chat_sent(self, data, protocol):
-        message = data['parsed']['message']
+    def on_chat_sent(self, data, connection):
+        """
+        Catch when someone sends a message.
+        :param data: The packet containing the message.
+        :param connection: The connection from which the packet came.
+        :return: Boolean. True if we're done with the packet here, False if the
+                 player is muted (preventing packet from being passed along.
+                 Commands are treated as truthy values.
+        """
+        message = data["parsed"]["message"]
         if message.startswith(
                 self.plugins.command_dispatcher.plugin_config.command_prefix):
             return True
-        if self.mute_check(protocol.player):
-            send_message(protocol, "You are muted and cannot chat.")
+        if self.mute_check(connection.player):
+            send_message(connection, "You are muted and cannot chat.")
             return False
         return True
 
-    @Command("mute", doc="Mutes a user",
-             syntax="(username)", role=MutePlayer)
-    def mute(self, data, protocol):
+    @Command("mute",
+             role=MutePlayer,
+             doc="Mutes a user",
+             syntax="(username)")
+    def mute(self, data, connection):
+        """
+        Mute command.
+        Pulls target's name from data stream. Check if valid player. Also check
+        if player can be muted, or is already muted. Otherwise, mute target.
+        """
         name = " ".join(data)
         player = self.plugins.player_manager.get_player_by_name(name)
         if player is None:
             raise NameError
         elif self.mute_check(player):
-            send_message(protocol, "%s is already muted." % player.name)
+            send_message(connection, "{} is already muted.".format(player.name))
             return
         elif player.check_role(Unmuteable):
-            send_message(protocol, "%s is unmuteable." % player.name)
+            send_message(connection, "{} is unmuteable.".format(player.name))
             return
         else:
             self.storage.mutes.add(player)
-            send_message(protocol, "%s has been muted." % player.name)
-            send_message(player.protocol,
-                         "%s has muted you." % protocol.player.name)
+            send_message(connection, "{} has been muted.".format(player.name))
+            send_message(player.connection,
+                         "{} has muted you.".format(connection.player.name))
 
     @Command("unmute",
+             role=UnmutePlayer,
              doc="Unmutes a player",
-             syntax="(username)",
-             role=UnmutePlayer)
-    def unmute(self, data, protocol):
+             syntax="(username)")
+    def unmute(self, data, connection):
+        """
+        Unmute command.
+        Pulls target's name from data stream. Check if valid player. Check that
+        player is actually muted. Otherwise, unmute target.
+        """
         name = " ".join(data)
         player = self.plugins.player_manager.get_player_by_name(name)
         if player is None:
             raise NameError
         elif not self.mute_check(player):
-            send_message(protocol, "%s isn't muted." % player.name)
+            send_message(connection, "{} isn't muted.".format(player.name))
             return
         else:
             self.storage.mutes.remove(player)
-            send_message(protocol, "%s has been unmuted." % player.name)
-            send_message(player.protocol,
-                         "%s has unmuted you." % protocol.player.name)
+            send_message(connection, "{} has been unmuted.".format(player.name))
+            send_message(player.connection,
+                         "{} has unmuted you.".format(connection.player.name))
 
     def mute_check(self, player):
+        """
+        Utility function for verifying target player's mute status.
+        :param player: Target player to check.
+        :return: Boolean. True if player is mutes, False if they are not.
+        """
         return player in self.storage.mutes
--- plugins/colored_names.py
+++ plugins/colored_names.py
@@ -1,31 +1,58 @@
+"""
+StarryPy Colored Names Plugin
+
+Provides colored names in the chat window for users based on role status. Does
+not effect players nametags directly. Also provides timestamps on messages, if
+so desired.
+
+Original authors: teihoo, FZFalzar
+Updated for release: kharidiron
+"""
+
+# TODO: Fundamental issue - by colorizing names, we prevent player's messages
+#       from being logged in the 'players' chat tab. This is because messages
+#       are sent using the 'broadcast' bit, with no username or client_id
+#       attached. Instead, the message body itself contains all the trappings:
+#       timestamp, username, colors and finally the message itself.
+
 import asyncio
+
 from utilities import DotDict
 from datetime import datetime
 from base_plugin import BasePlugin
 from utilities import ChatSendMode
 
 
-__author__ = 'teihoo, FZFalzar'
-
+###
 
 class ColoredNames(BasePlugin):
-    """
-    Plugin that brings colors to player names in the chat box.
-    """
     name = "colored_names"
-    depends = ['player_manager', 'command_dispatcher', 'chat_manager']
+    depends = ["player_manager", "command_dispatcher"]
+
+    def __init__(self):
+        super().__init__()
+        self.config = None
+        self.colors = {}
 
     def activate(self):
         super().activate()
         self.config = self.plugins.command_dispatcher.plugin_config
-        asyncio.Task(self.load_config())
+        asyncio.ensure_future(self.load_config())
 
     @asyncio.coroutine
     def load_config(self):
+        """
+        Utility function for loading colored names. Check if definitions exit in
+        config.json. If not, use defaults.
+        :return: DotDict. Contains Role:Color definitions.
+        """
+        # TODO: These roles are hard-coded. Ideally, we should let server admins
+        #       define their own roles and colors-per-role.
+        #       See StarryPy/StarryPy3k#4.
         try:
             self.colors = DotDict(self.config.config.colors)
-            self.logger.info("Configuration loaded!")
-        except:
+            self.logger.sender("Configuration loaded!")
+        except AttributeError:
             self.logger.warning(
                 "Failed to load from config! Initiating with default values")
             self.colors = DotDict({
@@ -37,33 +64,54 @@ class ColoredNames(BasePlugin):
                 "default": "^reset;"
             })
 
-    def on_chat_sent(self, data, protocol):
+    def on_chat_sent(self, data, connection):
+        """
+        Catch when someone sends a message. Add a timestamp to the message (if
+        that feature is turned on). Colorize the player's name based on their
+        role.
+        :param data: The packet containing the message.
+        :param connection: The connection from which the packet came.
+        :return: Boolean. True if an error occurred while generating a colored
+                 name (so that we don't stop the packet from flowing). Null if
+                 the message came from the server (since it doesn't need colors)
+                 or if the message is a command.
+        """
         message = data['parsed']['message']
         if not message.startswith(self.config.command_prefix):
             now = datetime.now()
             try:
+                # Check if option is set in config.json
                 if self.config.chattimestamps:
                     timestamp = "[{}]".format(now.strftime("%H:%M"))
                 else:
                     timestamp = ""
-            except:
+            except ValueError:
+                # If not, use the default case (True)
                 self.config.chattimestamps = True
                 timestamp = "[{}]".format(now.strftime("%H:%M"))
 
-            info = self.plugins['player_manager'].get_player_by_name(
-                protocol.player.name)
+            # Determine message sender for later
+            sender = self.plugins['player_manager'].get_player_by_name(
+                connection.player.name)
 
             try:
                 p = data['parsed']
-                if info.name == "server":
+                if sender.name == "server":
+                    # Server messages don't get colorized
                     return
+
+                # Colorize message based on SendMode [OBSOLETE]
+                # TODO: Only one chat channel now. Either this should be deleted
+                #       or chat-channels will need to be re-implemented in
+                #       StarryPy.
                 if p['send_mode'] == ChatSendMode.WORLD:
                     cts_color = "^green;"
                 elif p['send_mode'] == ChatSendMode.UNIVERSE:
-                    cts_color = "^yellow;"
+                    cts_color = "^yellow;"  # <- Starbound default color
                 else:
                     cts_color = "^gray;"
-                sender = self.colored_name(info)
+
+                sender = self.colored_name(sender)
                 msg = "{}{} <{}{}> {}".format(
                     cts_color,
                     timestamp,
@@ -71,24 +119,35 @@ class ColoredNames(BasePlugin):
                     cts_color,
                     p['message']
                 )
+
+                # Check if people are on the same planet. If so, and WORLD chat
+                # is enabled, send it only to them. Otherwise, send it to out
+                # to broadcast (to everyone).
                 if p['send_mode'] == ChatSendMode.WORLD:
                     for p in self.factory.protocols:
-                        if p.player.location == protocol.player.location:
+                        if p.player.location == connection.player.location:
                             yield from p.send_message(msg)
                 else:
                     yield from self.factory.broadcast(msg)
+
             except AttributeError as e:
                 self.logger.warning(
                     "AttributeError in colored_name: {}".format(str(e)))
-                yield from protocol.send_message(
+                cts_color = ""
+                yield from connection.send_message(
                     "{}<{}{}> {}".format(cts_color,
-                                         protocol.player.name,
+                                         connection.player.name,
                                          cts_color,
-                                         info.message))
+                                         sender.message))
                 return True
-        return False
+        return
 
     def colored_name(self, data):
+        """
+        Generate colored name based on target's role.
+        :param data: target to check against
+        :return: DotDict. Name of target will be colorized.
+        """
         if "Owner" in data.roles:
             color = self.colors.Owner
         elif "SuperAdmin" in data.roles:
