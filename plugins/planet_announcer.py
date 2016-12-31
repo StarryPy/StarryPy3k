5b582c626eb187cde8f88cb7299edae9df7e643b
medeor413 <medeor413@users.noreply.github.com>
2016-12-31 12:12:59 -0500

    Reimplement Planet Announcer plugin The planet announcer plugin announces the arrival of a player on a world to all players on that world. It also allows Admins to set a custom greeting message for anyone who enters a world.

---
+++ plugins/planet_announcer.py
@@ -0,0 +1,66 @@
+"""
+StarryPy Planet Announcer Plugin
+
+Announces to all players on a world when another player enters the world.
+Allows Admins to set a custom greeting message on a world.
+
+Reimplemented for StarryPy3k by medeor413.
+"""
+
+import asyncio
+
+from base_plugin import StorageCommandPlugin
+from plugins.player_manager import Admin
+from utilities import send_message, Command
+
+
+class PlanetAnnouncer(StorageCommandPlugin):
+    name = "planet_announcer"
+    depends = ["player_manager", "command_dispatcher"]
+
+    def __init__(self):
+        super().__init__()
+
+    def activate(self):
+        super().activate()
+        if "greetings" not in self.storage:
+            self.storage["greetings"] = {}
+
+    def on_world_start(self, data, connection):
+        asyncio.ensure_future(self._announce(connection))
+        return True
+
+    @asyncio.coroutine
+    def _announce(self, connection):
+        """
+        Announce to all players in the world when a new player beams in,
+        and display the greeting message to the new player, if set.
+
+        :param connection: The connection of the player beaming in.
+        :return: Null.
+        """
+        yield from asyncio.sleep(.5)
+        location = str(connection.player.location)
+        for p in self.factory.connections:
+            if str(p.player.location) == location and p != connection:
+                send_message(p, "{} has beamed down to the planet!".format(
+                    connection.player.alias))
+        if location in self.storage["greetings"]:
+            send_message(connection, self.storage["greetings"][location])
+
+    @Command("set_greeting",
+             role=Admin,
+             doc="Sets the greeting message to be displayed when a player "
+                 "enters this planet, or clears it if unspecified.")
+    def _set_greeting(self, data, connection):
+        location = str(connection.player.location)
+        msg = " ".join(data)
+        if not msg:
+            if location in self.storage["greetings"]:
+                self.storage["greetings"][location].pop()
+                yield from send_message(connection, "Greeting message "
+                                                    "cleared.")
+        else:
+            self.storage["greetings"][location] = msg
+            yield from send_message(connection, "Greeting message set to \"{}"
+                                                "\".".format(msg))
