c35fdc65bc46a0222bd0d48faecd3b9ffb1eac6b
eric vz <git@base10.org>
2018-11-01 23:23:36 -0500

    Fix paths in start script

--- docker-start.sh
+++ docker-start.sh
@@ -2,12 +2,12 @@
 
 # If we run in docker, and the user chose a file share volume, these files won't exist. Starry will complain.
 # Rather than change the way starry does its default files, let's just ensure they are present with this shell script.
-if [ -f /app/config/config.json.default ]; then
-    cp /app/defaults/config.json.default /app/config
+if [ ! -f config/config.json.default ]; then
+    cp defaults/config.json.default config
 fi
 
-if [ -f /app/config/permissions.json.default ]; then
-    cp /app/defaults/permissions.json.default /app/config
+if [ ! -f config/permissions.json.default ]; then
+    cp defaults/permissions.json.default config
 fi
 
 python3 ./server.py
