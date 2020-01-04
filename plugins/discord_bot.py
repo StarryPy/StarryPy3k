b4f6f0674517b88abfda4dc023d12dd7faf11834
eric vz <git@base10.org>
2020-01-04 00:29:02 -0600

    debug command to force a discord connection sever

--- plugins/discord_bot.py
+++ plugins/discord_bot.py
@@ -214,6 +214,8 @@ class DiscordPlugin(BasePlugin):
             msg = data["parsed"]["message"]
             if self.sc:
                 msg = self.color_strip.sub("", msg)
+            if msg == "!killdiscord": #FOR TESTING: REMOVE ME
+                asyncio.ensure_future(self.discord_client.logout())
             if data["parsed"]["send_mode"] == ChatSendMode.UNIVERSE:
                 if self.chat_manager:
                     if not self.chat_manager.mute_check(connection.player):
